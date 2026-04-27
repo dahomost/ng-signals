99c23aa61c44658544b75e32ac6fcdf8369204d0
dahomost <daho2020@gmail.com>
2026-04-27 13:16:24 -0400

    Prettier config

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,7 @@
 import { Routes } from '@angular/router';
 import { SignupComponent } from './features/signup/signup.component';
 
-export const routes: Routes = [{ path: '', pathMatch: 'full', redirectTo: 'signup' }, { path: 'signup', component: SignupComponent }];
+export const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'signup' },
+  { path: 'signup', component: SignupComponent },
+];
